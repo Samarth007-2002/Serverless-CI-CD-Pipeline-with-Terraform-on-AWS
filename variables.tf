c9209f5e1a5bf9e070e3092b732cd6178021ba3a
Samarth007-2002 <142674758+Samarth007-2002@users.noreply.github.com>
2024-08-14 15:46:20 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,3 +9,11 @@ variable "state_bucket_name" {
 variable "artifact_bucket_name" {
   default = "terraform-codepipeline-artificat-bucket2"
 }
+
+variable "repo_id" {
+  default = "Samarth007-2002/terraform-CI-CD_SOURCE" 
+}
+
+variable "branch_name" {
+  default = "master"
+}
